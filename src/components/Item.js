d6e0d10c6cf730d87ed3ab61b3dc918b1136ac08
LingZhouChen <lingzhouc@gmail.com>
2023-10-06 14:10:40 -0400

    add to cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false)
+
+  function addCart() {
+    setCart(inCart => inCart = !inCart)
+  }
+
+  const cartClass = inCart ? "in-cart" : "";
+  const cartBtn = inCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addCart} className="add">{cartBtn}</button>
     </li>
   );
 }
