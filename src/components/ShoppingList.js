4319e1c90cc8af41d8319ae9dcf3907b4729eefd
LingZhouChen <lingzhouc@gmail.com>
2023-10-06 16:18:09 -0400

    add filter and complete lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  // keep track of selected 
+  const [selectedCategory, setSelected] = useState("All")
+
+  function selectCategory(event) {
+    setSelected(event.target.value)
+  }
+
+  const filteredOptions = items.filter(
+    (item) => item.category === selectedCategory)
+  
+  const food = selectedCategory === "All" ?  
+    items.map((item) => (<Item key={item.id} name={item.name} category={item.category} />))  
+    : filteredOptions.map((item) => (<Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select value={selectedCategory} onChange={selectCategory} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {food}
       </ul>
     </div>
   );
